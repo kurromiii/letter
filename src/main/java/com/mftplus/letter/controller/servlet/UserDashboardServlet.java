2134f0d93b6226ec9295f95749f3ef8bcdca6e15
kurromii <ahadzadeparnian@gmail.com>
2024-05-15 21:13:47 +0330

    user dashboard servlet and jsp added

--- src/main/java/com/mftplus/letter/controller/servlet/UserDashboardServlet.java
+++ src/main/java/com/mftplus/letter/controller/servlet/UserDashboardServlet.java
@@ -9,15 +9,15 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/dashboard.do")
+@WebServlet(urlPatterns = "/userDashboard.do")
 @Slf4j
-public class DashboardServlet extends HttpServlet {
+public class UserDashboardServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.info("DashBoardServlet - Get");
+        log.info("UserDashBoardServlet - Get");
         try {
-            req.getRequestDispatcher("/jsp/dashboard.jsp").forward(req, resp);
+            req.getRequestDispatcher("/jsp/user-dashboard.jsp").forward(req, resp);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
